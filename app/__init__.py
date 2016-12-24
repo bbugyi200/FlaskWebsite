7dde062950395f81b2d4002c563ede2715f43acb
Bryan Bugyi <bryan_bugyi@mymail.rcbc.edu>
2016-12-24 00:29:12 -0500

    Static_URL_Path

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/app/static')
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
