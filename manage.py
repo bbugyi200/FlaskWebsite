d49b219966f2ca3b53acbbc0afc22d1b6726fd70
Bryan Bugyi <bryan_bugyi@mymail.rcbc.edu>
2016-12-24 17:24:20 -0500

    Replaced one image with four

--- manage.py
+++ manage.py
@@ -1,9 +1,32 @@
 # import os
 from app import create_app  # , db
 from flask_script import Manager  # , Shell
-from flask import Flask
+from flask import Flask, url_for
+import os
 
 app = create_app('default')
+
+
+###################################
+#  CACHE BUSTER FOR STATIC FILES  #
+###################################
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+##################################
+
 manager = Manager(app)
 
 if __name__ == '__main__':
