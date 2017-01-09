0508b69399ad4d7e9413d9fa7a6c3edf736c69f4
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-01-09 00:32:45 -0500

    Added --filter option

--- reportusage.py
+++ reportusage.py
@@ -1,21 +1,33 @@
 from __future__ import print_function
 from manage import UsageData
 from datetime import timedelta
+from sys import argv
 import sys
 
 sys.stdout = open('usagedata.txt', 'w')
 
+badIPs = {'home': '173.72.47.206'}
+
+Filter  = False
+
+try:
+    if argv[1] == '--filter':
+        Filter = True
+except IndexError:
+    pass
+
 for hit in UsageData.get_usage():
-    # DATE
-    date = hit['date']
-    td = timedelta(hours=-5)
-    date += td
-    fmtdate = date.strftime('%m/%d/%Y - %H:%M:%S')
-    print(fmtdate)
-    hit.pop('date')
+    if not Filter or (hit['remote_addr'] not in badIPs.values()):
+        # DATE
+        date = hit['date']
+        td = timedelta(hours=-5)
+        date += td
+        fmtdate = date.strftime('%m/%d/%Y - %H:%M:%S')
+        print(fmtdate)
+        hit.pop('date')
 
-    for key, item in hit.items():
-        print('{0}: {1}'.format(key, item))
+        for key, item in hit.items():
+            print('{0}: {1}'.format(key, item))
 
-    # SPACE
-    print()
+        # SPACE
+        print()
