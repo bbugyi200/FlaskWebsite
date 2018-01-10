f1c2ca05125503b377d12cf5b63044987c8faae6
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-01-09 19:58:30 -0500

    Migrate from apscheduler to threading.Timer

--- start.py
+++ start.py
@@ -4,14 +4,12 @@ where it all begins...
 """
 
 import os
-import atexit
 import urllib.request
+from threading import Timer
 
 os.environ['FLASK_APP_CONFIG'] = 'production'
 
 from manage import app
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 from werkzeug.debug import DebuggedApplication
 
 # Enabling debugging
@@ -23,25 +21,17 @@ app.debug = True
 #  Sets up Job to Fetch cv.pdf Daily  #
 #######################################
 
+this_dir = os.path.abspath(os.path.dirname(__file__))
+cvPath = this_dir + "/app/static/resume/cv.pdf"
+
+
 def fetchCVFromGitHub():
-    this_dir = os.path.abspath(os.path.dirname(__file__))
-    target = this_dir + "/app/static/resume/cv.pdf"
     resp = urllib.request.urlopen("http://github.com/bbugyi200/CV/raw/master/cv.pdf")
 
-    file = open(target, 'wb')
+    file = open(cvPath, 'wb')
     file.write(resp.read())
     file.close()
-
-
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(func=fetchCVFromGitHub,
-                  trigger=IntervalTrigger(hours=6),
-                  id='fetch_cv_job',
-                  name='Fetch cv.pdf from GitHub daily',
-                  repace_existing=True)
-
-atexit.register(lambda: scheduler.shutdown())
+    Timer(10800, fetchCVFromGitHub).start()
 
 
 if __name__ == '__main__':
